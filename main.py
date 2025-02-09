c77c4a42ea4d83267404221df83afcae4f07ec39
choushen <jacob.mckenzie@icloud.com>
2025-02-09 19:09:15 +0000

    Added basic layout to the window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from typing import List
-from PySide6.QtWidgets import (QApplication, QComboBox, QDialog,
+from PySide6.QtWidgets import (QApplication, QWidget, QComboBox, QDialog,
                                QDialogButtonBox, QGridLayout, QGroupBox,
                                QFormLayout, QHBoxLayout, QLabel, QLineEdit,
                                QMenu, QMenuBar, QPushButton, QSpinBox,
@@ -18,10 +18,20 @@ class MainWindow(QMainWindow):
         main_widget: QWidget = QWidget()
 
         # Create the layout
-        self.setCentralWidget(central_widget)
+        self.setCentralWidget(main_widget)
 
         layout: QVBoxLayout = QVBoxLayout()
         main_widget.setLayout(layout)
+
+        # Load components
+        self.workshop_link_input_box: WorkshopLinkInputBox = WorkshopLinkInputBox()
+        self.edit_file_btn_: EditFileButton = EditFileButton()
+        self.output_mod_list_btn: OutputModListButton = OutputModListButton()
+
+        # Add components to the layout
+        layout.addWidget(self.workshop_link_input_box)
+        layout.addWidget(self.edit_file_btn_)
+        layout.addWidget(self.output_mod_list_btn)
         
 
 class WorkshopLinkInputBox(QLineEdit):
@@ -42,9 +52,10 @@ class OutputModListButton(QPushButton):
 
 
 def main():
-    print("Hello World")
-    MainWindow()
-    app: QApplication = QApplication(sys.argv) # Creates the application event loop
+    app: QApplication = QApplication(sys.argv)  # Creates the application event loop
+    window: MainWindow = MainWindow()  # Create the main window
+    window.show()  # Show the main window
+    sys.exit(app.exec())  # Start the event loop
 
 
 
